361ab5e0cb201232e8487c390012476cae383e08
mateja <mateja176@gmail.com>
2019-11-08 20:54:58 +0100

    feat: displays table

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,58 @@
-import csv from 'csvtojson';
+import parse from 'csv-parse';
+import { startCase } from 'lodash';
 import React from 'react';
 import './App.css';
 
+const borderStyle: React.CSSProperties = {
+  border: '1px solid #ccc',
+  borderCollapse: 'collapse',
+};
+
+const fieldStyle: React.CSSProperties = { whiteSpace: 'nowrap', padding: 5 };
+
 const App: React.FC = () => {
-  const [ufcFightHistory, setUfcFightHistory] = React.useState({});
+  const [ufcFightHistory, setUfcFightHistory] = React.useState([[]]);
 
   React.useEffect(() => {
     fetch('/data/ufc-fight-history.csv')
       .then(res => res.text())
-      .then(
-        text =>
-            csv().fromString(text),
-      )
-      .then(setUfcFightHistory);
+      .then(text =>
+        parse(text, (err, data) => {
+          if (!err) {
+            setUfcFightHistory(data.slice(0, 100));
+          }
+        }),
+      );
   }, []);
 
-  return <pre>{JSON.stringify(ufcFightHistory, null, 2)}</pre>;
+  return (
+    <div style={{ margin: 20 }}>
+      <table style={borderStyle}>
+        <thead>
+          {ufcFightHistory.slice(0, 1).map((row, i) => (
+            <tr key={i}>
+              {row.map((heading, j) => (
+                <th key={j} style={{ ...borderStyle, ...fieldStyle }}>
+                  {startCase(heading)}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {ufcFightHistory.slice(1).map((row, i) => (
+            <tr key={i} style={{ background: i % 2 ? '#eee' : 'white' }}>
+              {row.map((field, j) => (
+                <td key={j} style={{ ...borderStyle, ...fieldStyle }}>
+                  {field}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default App;
