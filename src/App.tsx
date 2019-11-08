df4b68c7782549ec3c8f9e2b464f1bb65b86c143
mateja <mateja176@gmail.com>
2019-11-08 20:05:54 +0100

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
+import csv from 'csvtojson';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [ufcFightHistory, setUfcFightHistory] = React.useState({});
+
+  React.useEffect(() => {
+    fetch('/data/ufc-fight-history.csv')
+      .then(res => res.text())
+      .then(
+        text =>
+            csv().fromString(text),
+      )
+      .then(setUfcFightHistory);
+  }, []);
+
+  return <pre>{JSON.stringify(ufcFightHistory, null, 2)}</pre>;
+};
 
 export default App;
